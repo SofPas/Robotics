a0a18df02643eb53b8806c46afc487cbdcf4c15c
SofPas <pasiasofia10@gmail.com>
2025-10-12 19:48:52 +0300

    Create button_led.ino

---
+++ arduino/led/button_LED/button_led.ino
@@ -0,0 +1,16 @@
+int LED_PIN = 10;
+int BUTTON_PIN = 2;
+
+void setup() {
+  pinMode (LED_PIN, OUTPUT);
+  pinMode (BUTTON_PIN, INPUT);
+}
+
+void loop() {
+  if (digitalRead (BUTTON_PIN) == HIGH) {
+    digitalWrite (LED_PIN, HIGH);
+  }
+  else {
+    digitalWrite (LED_PIN, LOW);
+  }
+}
