a4b80bed067731adc5db0cff7ed3845f3a262f08
SofPas <pasiasofia10@gmail.com>
2025-10-26 15:52:07 +0200

    Create seven_segment.ino

---
+++ arduino/led/7-segment_display/seven_segment.ino
@@ -0,0 +1,147 @@
+int E = 7;
+int D = 5;
+int C = 4;
+int DP = 6;
+int B = 3;
+int A = 2;
+int F = 8;
+int G = 9;
+
+void setup() {
+  pinMode (E, OUTPUT);
+  pinMode (D, OUTPUT);
+  pinMode (C, OUTPUT);
+  pinMode (DP, OUTPUT);
+  pinMode (B, OUTPUT);
+  pinMode (A, OUTPUT);
+  pinMode (F, OUTPUT);
+  pinMode (G, OUTPUT);
+}
+
+void one() {
+  digitalWrite (E, LOW);
+  digitalWrite (D, LOW);
+  digitalWrite (C, HIGH);
+  digitalWrite (DP, LOW);
+  digitalWrite (B, HIGH);
+  digitalWrite (A, LOW);
+  digitalWrite (F, LOW);
+  digitalWrite (G, LOW);
+}
+
+void two() {
+  digitalWrite (E, HIGH);
+  digitalWrite (D, HIGH);
+  digitalWrite (C, LOW);
+  digitalWrite (DP, LOW);
+  digitalWrite (B, HIGH);
+  digitalWrite (A, HIGH);
+  digitalWrite (F, LOW);
+  digitalWrite (G, HIGH);
+}
+
+void three() {
+  digitalWrite (E, LOW);
+  digitalWrite (D, HIGH);
+  digitalWrite (C, HIGH);
+  digitalWrite (DP, LOW);
+  digitalWrite (B, HIGH);
+  digitalWrite (A, HIGH);
+  digitalWrite (F, LOW);
+  digitalWrite (G, HIGH);
+}
+
+void four() {
+  digitalWrite (E, LOW);
+  digitalWrite (D, LOW);
+  digitalWrite (C, HIGH);
+  digitalWrite (DP, LOW);
+  digitalWrite (B, HIGH);
+  digitalWrite (A, LOW);
+  digitalWrite (F, HIGH);
+  digitalWrite (G, HIGH);
+}
+
+void five() {
+  digitalWrite (E, LOW);
+  digitalWrite (D, HIGH);
+  digitalWrite (C, HIGH);
+  digitalWrite (DP, LOW);
+  digitalWrite (B, LOW);
+  digitalWrite (A, HIGH);
+  digitalWrite (F, HIGH);
+  digitalWrite (G, HIGH);
+}
+
+void six() {
+  digitalWrite (E, HIGH);
+  digitalWrite (D, HIGH);
+  digitalWrite (C, HIGH);
+  digitalWrite (DP, HIGH);
+  digitalWrite (B, LOW);
+  digitalWrite (A, HIGH);
+  digitalWrite (F, HIGH);
+  digitalWrite (G, HIGH);
+}
+
+void seven() {
+  digitalWrite (E, LOW);
+  digitalWrite (D, LOW);
+  digitalWrite (C, HIGH);
+  digitalWrite (DP, LOW);
+  digitalWrite (B, HIGH);
+  digitalWrite (A, HIGH);
+  digitalWrite (F, LOW);
+  digitalWrite (G, LOW);
+}
+
+void eight() {
+  digitalWrite (E, HIGH);
+  digitalWrite (D, HIGH);
+  digitalWrite (C, HIGH);
+  digitalWrite (DP, LOW);
+  digitalWrite (B, HIGH);
+  digitalWrite (A, HIGH);
+  digitalWrite (F, HIGH);
+  digitalWrite (G, HIGH);
+}
+
+void nine() {
+  digitalWrite (E, LOW);
+  digitalWrite (D, LOW);
+  digitalWrite (C, HIGH);
+  digitalWrite (DP, HIGH);
+  digitalWrite (B, HIGH);
+  digitalWrite (A, HIGH);
+  digitalWrite (F, HIGH);
+  digitalWrite (G, HIGH);
+}
+
+void loop() {
+  one();
+  delay(1000);
+
+  two();
+  delay(1000);
+
+  three();
+  delay(1000);
+
+  four();
+  delay(1000);
+
+  five();
+  delay(1000);
+
+  six();
+  delay(1000);
+
+  seven();
+  delay(1000);
+
+  eight();
+  delay(1000);
+
+  nine();
+  delay(1000);
+}
